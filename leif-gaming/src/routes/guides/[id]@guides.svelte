00b76e70cc272682665df3366fa4a355eeec8822
leifeggenfellner <eggenfellner@protonmail.com>
2022-04-18 17:05:02 +0100

    added routing

--- leif-gaming/src/routes/guides/[id]@guides.svelte
+++ leif-gaming/src/routes/guides/[id]@guides.svelte
@@ -1,9 +1,8 @@
 <script context="module">
 	export async function load({ fetch, params }) {
-		await new Promise((resolve) => setTimeout(resolve, 1000));
 		const id = params.id;
-		const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-		const guide = await res.json();
+		const res = await fetch(`/guides/${id}.json`);
+		const { guide } = await res.json();
 
 		if (res.ok) {
 			return {
